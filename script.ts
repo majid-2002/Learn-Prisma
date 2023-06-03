339c615a36228e77ba7d23204ac17c16c2d421cc
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-06-03 13:12:18 +0530

    Implemented Delete, Create, Read, Update operations on schema

--- script.ts
+++ script.ts
@@ -2,22 +2,55 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
-
-  const user = await prisma.user.create({
+  const users = await prisma.user.update({
     data: {
-      name: "majid",
-      age: 18,
-      email: "majid@test.com",
-      userPreference: {
-        create: {
-          emailUpdates: true,
-        },
-      },
+      email: "alicepen@13.com",
     },
-    include: { userPreference: true },
+    where: {
+      email: "alicd@124.com",
+    },
   });
-  console.log(user);
+
+  // const users = await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       name: "Alice",
+  //       email: "alicd@124.com",
+  //       age: 23,
+  //     },
+  //   ],
+  // });
+
+  // const users = await prisma.user.findMany({
+  //   where: {
+  //     name: "Alice",
+  //   },
+  //   // take: 1, //? take 1 record
+  //   // skip: 1, //? skip 1 record
+  //   orderBy: {
+  //     age: "desc", //? order by age ascending or descending
+  //   },
+  // });
+
+  // const users = await prisma.user.findMany({
+  //   where: {
+  //     // name: {
+  //     //   equals: "Alice", //? equal to Alice
+  //     //   not: "Alice", //? not equal to Alice
+  //     //   in: ["Alice", "Bob"], //? gives all the users in the array
+  //     //   notIn: ["Alice", "Bob"], //? gives all the users not in the array
+  //     // },
+  //     age: {
+  //       lt: 30, //? less than 30
+  //     },
+  //     email: {
+  //       contains: "alic", //? contains alic in the email
+  //       endsWith: "124.com", //? ends with 124.com
+  //     },
+  //   },
+  // });
+
+  console.log(users);
 }
 
 main()
